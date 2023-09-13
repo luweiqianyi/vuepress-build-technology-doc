c1003b29cd2f6bec94e60046ef4c989cbc909e08
LuWeiQianYi <runningriven@163.com>
2023-09-13 11:49:52 +0800

    refactor: 目录结构调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,20 +5,35 @@ module.exports = {
         siderbar: 'auto',
         nav: [
             { text: '主页', link: '/' },
-            { text: 'go', link: '/go/01-go-GMP.html' },
-            { text: 'go-zero', link: '/go-zero/01-quick-start.html' },
-            { text: 'Docker', link: '/docker/00-docker-image-build.html' },
-            { text: 'Linux', link: '/linux/00-linux-directory.html' },
-            { text: 'vuepress搭建文档教程', link: '/vuepress-tutorial/00-website-init.html' },
-            { text: 'TLS', link: '/tls/00-make-cert.html' },
-            { text: 'redis', link: '/redis/01-pub-sub.html' },
-            { 
-                text: 'distribute', 
-                items:[
-                    { text: 'minio', link: '/distribute/minio/01-minIO.html' },
-                    { text: 'seta', link: '/distribute/seta/01-distribute-transaction-mode.html' },
+            {
+                text: 'go',
+                items: [
+                    { text: 'go', link: '/go/01-go-GMP.html' },
+                    { text: 'go-zero', link: '/go-zero/01-quick-start.html' },
                 ]
             },
+            {
+                text: 'os',
+                items: [
+                    { text: 'Docker', link: '/docker/00-docker-image-build.html' },
+                    { text: 'Linux', link: '/linux/00-linux-directory.html' },
+                ]
+            },
+            {
+                text: 'cryptography',
+                items: [
+                    { text: 'TLS', link: '/tls/00-make-cert.html' },
+                ]
+            },
+            {
+                text: 'distribute',
+                items: [
+                    { text: 'minio', link: '/distribute/minio/01-minIO.html' },
+                    { text: 'seta', link: '/distribute/seta/01-distribute-transaction-mode.html' },
+                    { text: 'redis', link: '/redis/01-pub-sub.html' },
+                ]
+            },
+            { text: 'vuepress', link: '/vuepress-tutorial/00-website-init.html' },
         ]
     }
 }
